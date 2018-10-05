9efc7436dbe5921c8abbc24cdc0fdf6c8ac3ab1e
tarragoesteve <tarragoesteve@gmail.com>
2018-10-06 01:02:06 +0200

    organaized index.js

--- public/index.js
+++ public/index.js
@@ -1,5 +1,8 @@
-console.log("Hello");
-var railmap = L.map('map').setView([51.505, -0.09], 13);
+var railmap = L.map('map').setView([41.344161, 2.098366], 13);
+/*
+MAP BACKGROUND
+*/
+
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -7,6 +10,13 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1Ijoia2l0dHVzIiwiYSI6ImNqbXdsMzlwMTM5MDEzcG54bXdrM281anoifQ.bm1LajQFRW9BGEe2iq8kYQ'
 }).addTo(railmap);
 
+/*
+STOPS
+*/
+
+/*
+TRAINRAILS
+*/
 var polyline = L.polyline([
     [51.509, -0.08],
     [51.503, -0.06],
@@ -19,3 +29,7 @@ var decorator = L.polylineDecorator(polyline, {
         {offset: 0, repeat: 20, symbol: L.Symbol.dash({pixelSize: 10})}
     ]
 }).addTo(railmap);
+
+/*
+TRAINS
+*/
