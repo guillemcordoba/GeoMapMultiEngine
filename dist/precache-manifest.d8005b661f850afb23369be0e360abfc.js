57f1e98767699dddd4fa5c2c2647c087cc4e3be1
Guillem Cordoba <guillem.cordoba@gmail.com>
2018-10-05 22:16:49 +0200

    Deploy

---
+++ dist/precache-manifest.d8005b661f850afb23369be0e360abfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "83ff2ee32db5f00d7925",
+    "url": "/js/chunk-vendors.bb3a6f6d.js"
+  },
+  {
+    "revision": "ba3049bf5965f53d0ee8",
+    "url": "/js/app.24b91032.js"
+  },
+  {
+    "revision": "00ca461b031609f415e1262b939a3e50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ba3049bf5965f53d0ee8",
+    "url": "/css/app.f5fba5f5.css"
+  }
+];
