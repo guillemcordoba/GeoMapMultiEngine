08124eeb374adecc6fb0518e10697f296d136378
Kittus <markasenjo@hotmail.com>
2018-10-06 12:08:33 +0200

    zoomed labels again

--- public/index.js
+++ public/index.js
@@ -8,23 +8,45 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1Ijoia2l0dHVzIiwiYSI6ImNqbXdsMzlwMTM5MDEzcG54bXdrM281anoifQ.bm1LajQFRW9BGEe2iq8kYQ'
 }).addTo(railmap);
 
+var lastZoom;
+var tooltipThreshold = 15;
+
+railmap.on('zoomend', function() {
+    var zoom = railmap.getZoom();
+    if (zoom < tooltipThreshold && (!lastZoom || lastZoom >= tooltipThreshold)) {
+        railmap.eachLayer(function(l) {
+            if (l.getTooltip()) {
+                var tooltip = l.getTooltip();
+                l.unbindTooltip().bindTooltip(tooltip, {
+                    permanent: false
+                })
+            }
+        })
+    } else if (zoom >= tooltipThreshold && (!lastZoom || lastZoom < tooltipThreshold)) {
+        railmap.eachLayer(function(l) {
+            if (l.getTooltip()) {
+                var tooltip = l.getTooltip();
+                l.unbindTooltip().bindTooltip(tooltip, {
+                    permanent: true
+                })
+            }
+        });
+    }
+    lastZoom = zoom;
+})
+
 /**
  * STOPS
  */
 var stopIcon = L.icon({
     iconUrl: 'img/stop.png',
-
-     iconSize:     [12, 12], // size of the icon
-/*    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
- */});
-
+    iconSize:     [12, 12]
+});
 
 wagons = []
 routes = {}
 
-loadRouteData('data/stops/routes.json')
+loadRouteData('data/stops/routesTMB.json')
 
 function loadRouteData(filePath) {
   loadJSON(filePath, function(file) {
@@ -52,8 +74,11 @@ function drawElements(){
             latlong = [stop["stop_lat"], stop["stop_lon"]];
             coords.push(latlong);
 
-            L.marker(latlong, {icon: stopIcon}).addTo(railmap);
-
+            marker = L.marker(latlong, {icon: stopIcon}).addTo(railmap);
+            (railmap.getZoom() >= tooltipThreshold
+                ? marker.bindTooltip(stop["stop_name"], {permanent:true, direction: "top"})
+                : marker.bindTooltip(stop["stop_name"], {permanent:false, direction: "top"})
+            );
         }
 
         polylines[route] = L.polyline(coords,{color: 'black'}).addTo(railmap);
@@ -88,40 +113,96 @@ TRAINS
 */
 
 
+
 function loadJSON(filePath, success, error) {
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function()
-	{
-		if (xhr.readyState === XMLHttpRequest.DONE) {
-			if (xhr.status === 200) {
-				if (success)
-					success(JSON.parse(xhr.responseText));
-		} else {
-			if (error)
-				error(xhr);
-			}
-		}
-	};
-	xhr.open("GET", filePath, true);
-	xhr.send();
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function()
+    {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                if (success)
+                    success(JSON.parse(xhr.responseText));
+        } else {
+            if (error)
+                error(xhr);
+            }
+        }
+    };
+    xhr.open("GET", filePath, true);
+    xhr.send();
 }
 
 
+function weightedMean(pos1, pos2, weight) {
+  return [pos1[0] * (1-weight) + pos2[0] * weight,
+          pos1[1] * (1-weight) + pos2[1] * weight];
+
+}
+
+addTime(2*60*60)
+reDrawTrains()
+
+function reDrawTrains() {
+  var wagons = getWagons()
+  for(var wagon of wagons){
+    if (wagon.marker){
+
+    } else
+    {
+      var greenIcon = L.icon({
+      iconUrl: 'img/trains/'+routes[wagon.route].route_short_name+'.png',
+      iconSize:     [40, 20], // size of the icon
+      iconAnchor:   [20, 10], // point of the icon which will correspond to marker's location
+      popupAnchor:  [0, 15] // point from which the popup should open relative to the iconAnchor
+      });
+
+      wagon.marker = L.marker(weightedMean([wagon.start.stop_lat,wagon.start.stop_lon]
+       [wagon.end.stop_lat,wagon.end.stop_lon],wagon.time_to_arrive/wagon.total_time)
+       , {icon: greenIcon}).addTo(map);
+    }
+  }
+
+}
 /*
 Return dynamic information
 */
 
 
-var seconds_from_midnight = 8 * 60 * 60;
+var seconds_from_midnight = 4 * 60 * 60;
 function  getTime() {
   return seconds_from_midnight
+}
+
+
+function hour_to_seconds(hour) {
+  var hours = hour[0]*10+ hour[1]
+  var minutes = hour[3]*10+ hour[4]
+  var seconds = hour[6]*10+ hour[7]
+  return seconds + 60 * (minutes + hours * 60)
 }
 
 function addTime(time_to_add) {
   var next_time = seconds_from_midnight + time_to_add
   for (route of routes)
   {
-
+    var freq = 0
+    for (var time in route.freq) {
+      if (hour_to_seconds(time) < seconds_from_midnight){
+        freq = route.freq[time]
+      }
+    }
+    var freq = route.freq
+    for(var now=seconds_from_midnight; now<=next_time; ++now)
+    {
+      if (now%freq == 0 )
+      {
+        wagons.push({
+          "route": route,
+          "spawned_time" : now,
+          "start": 0
+        })
+      }
+    }
   }
 }
 
@@ -133,13 +214,27 @@ function getRoutes(){
 
 
 function getWagons() {
-  return
-    [{
-      "start": "1.41.1",
-      "end":  "1.41.1",
-      "route": "route1",
-      "time_to_arrive": 5,
-      "total_time": 10
+  for (var wagon_key in  wagons){
+    var wagon = wagons[wagon_key];
+    var routeKey = wagon.route
+    for (var stop of routes[routeKey].stops){
+      wagon.start = ""
+      wagon.end = ""
+      if((seconds_from_midnight - wagon.spawned_time) < hour_to_seconds(stop.departure_time))
+      {
+        wagon.start = stop
+      }
+      if(wagon.end == "" && (seconds_from_midnight - wagon.spawned_time) > hour_to_seconds(stop.arrival_time) )
+      {
+        wagon.end = stop
+        wagon.time_to_arrive = stop.arrival_time - seconds_from_midnight
+        wagon.total_time = stop.arrival_time - wagon.start.departure_time
+      }
     }
-    ]
+
+    if (wagon.end == ""){
+      delete wagons[wagon_key];
+    }
+  }
+  return wagons
 }
